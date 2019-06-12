b3a5e0bec253fb3f23d4b09404e10989a3e200cf
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2019-06-12 23:08:38 +0300

    1206
    זום וגלילה בבוק

--- jScripts/JavaScript.js
+++ jScripts/JavaScript.js
@@ -3,11 +3,13 @@ var wwind = window.innerWidth;
 document.documentElement.style.setProperty('--hwwind', hwind + "px");
 document.documentElement.style.setProperty('--wwwind', wwind-17 + "px");
 
-if (window.innerWidth > 1384) {
+if (window.innerWidth > 1384 && window.innerWidth < 2500) {
     document.documentElement.style.setProperty('--wwwind', 1287 + "px");
     var zoomn = window.innerWidth / 1300;
     document.documentElement.style.setProperty('--zoomi', zoomn );
 }
+
+
 
 window.onresize = function () {
     var hwind = window.innerHeight;
@@ -74,88 +76,77 @@ function fadd() {
 
 
 }
-//setTimeout(function () { window.onscroll = fundd(); }, 2000);
-var x = 0;
-var lastScrollTop = 0;
+
 function fundd() {
-            var nowScrollTop = $(this).scrollTop();
-                if (nowScrollTop > lastScrollTop) {
-x+=1
-                } else {
-x-=1
-                }
-                lastScrollTop = nowScrollTop;
-
-
-    if (x == 53) {
-        x=0
+    var ScrollyTop = window.scrollY
+    while (ScrollyTop > 650) {
+        ScrollyTop = ScrollyTop-650
     }
-    if (x == (-1)) {
-        x=52
-    }
-        if (x % 52 == 0) {
-            fadd();
-            document.getElementById('arak1').style.opacity = "1"
-        }
-        else {
-            if (x % 48 == 0) {
+
+            if (ScrollyTop % 600 < 50) {
                 fadd();
-                document.getElementById('arak2').style.opacity = "1"
+                document.getElementById('arak1').style.opacity = "1"
             }
             else {
-                if (x % 44 == 0) {
+                if (ScrollyTop % 550 < 50) {
                     fadd();
-                    document.getElementById('arak3').style.opacity = "1"
+                    document.getElementById('arak2').style.opacity = "1"
                 }
                 else {
-                    if (x % 40 == 0) {
+                    if (ScrollyTop % 500 < 50) {
                         fadd();
-                        document.getElementById('arak4').style.opacity = "1"
+                        document.getElementById('arak3').style.opacity = "1"
                     }
                     else {
-                        if (x % 36 == 0) {
+                        if (ScrollyTop % 450 < 50) {
                             fadd();
-                            document.getElementById('arak5').style.opacity = "1"
+                            document.getElementById('arak4').style.opacity = "1"
                         }
                         else {
-                            if (x % 32 == 0) {
+                            if (ScrollyTop % 400 < 50) {
                                 fadd();
-                                document.getElementById('arak6').style.opacity = "1"
+                                document.getElementById('arak5').style.opacity = "1"
                             }
                             else {
-                                if (x % 28 == 0) {
+                                if (ScrollyTop % 350 < 50) {
                                     fadd();
-                                    document.getElementById('arak7').style.opacity = "1"
+                                    document.getElementById('arak6').style.opacity = "1"
                                 }
                                 else {
-                                    if (x % 24 == 0) {
+                                    if (ScrollyTop % 300 < 50) {
                                         fadd();
-                                        document.getElementById('arak8').style.opacity = "1"
+                                        document.getElementById('arak7').style.opacity = "1"
                                     }
                                     else {
-                                        if (x % 20 == 0) {
+                                        if (ScrollyTop % 250 < 50) {
                                             fadd();
-                                            document.getElementById('arak9').style.opacity = "1"
+                                            document.getElementById('arak8').style.opacity = "1"
                                         }
                                         else {
-                                            if (x % 16 == 0) {
+                                            if (ScrollyTop % 200 < 50) {
                                                 fadd();
-                                                document.getElementById('arak10').style.opacity = "1"
+                                                document.getElementById('arak9').style.opacity = "1"
                                             }
                                             else {
-                                                if (x % 12 == 0) {
+                                                if (ScrollyTop % 150 < 50) {
                                                     fadd();
-                                                    document.getElementById('arak11').style.opacity = "1"
+                                                    document.getElementById('arak10').style.opacity = "1"
                                                 }
                                                 else {
-                                                    if (x % 8 == 0) {
+                                                    if (ScrollyTop % 100 < 50) {
                                                         fadd();
-                                                        document.getElementById('arak12').style.opacity = "1"
+                                                        document.getElementById('arak11').style.opacity = "1"
                                                     }
                                                     else {
-                                                        if (x % 4 == 0) {
+                                                        if (ScrollyTop % 50 < 50) {
                                                             fadd();
-                                                            document.getElementById('arak13').style.opacity = "1"
+                                                            document.getElementById('arak12').style.opacity = "1"
+                                                        }
+                                                        else {
+                                                            if (ScrollyTop < 50) {
+                                                                fadd();
+                                                                document.getElementById('arak13').style.opacity = "1"
+                                                            }
                                                         }
                                                     }
                                                 }
@@ -169,6 +160,4 @@ x-=1
                 }
             }
         }
-    }
-
-
+   
